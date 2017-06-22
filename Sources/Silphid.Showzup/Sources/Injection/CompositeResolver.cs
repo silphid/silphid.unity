ebc89718952ac2d0e5c3c1ba3c13f921387e9bf9
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-06-22 15:00:56 -0400

    Working on Container with List bindings.

---
+++ Sources/Silphid.Showzup/Sources/Injection/CompositeResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Silphid.Extensions;
+
+namespace Silphid.Showzup.Injection
+{
+    public class CompositeResolver : IResolver
+    {
+        private readonly IResolver[] _resolvers;
+
+        public CompositeResolver(params IResolver[] resolvers)
+        {
+            _resolvers = resolvers;
+        }
+
+        public object Resolve(Type abstractionType, IResolver subResolver = null, bool isOptional = false) =>
+            _resolvers
+                .WhereNotNull()
+                .Select(x => x.Resolve(abstractionType, subResolver, isOptional))
+                .FirstNotNullOrDefault();
+    }
+}
