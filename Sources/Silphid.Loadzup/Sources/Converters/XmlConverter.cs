c7b457f8bd2fcc79ea66a2fb1628f008e74741b8
dracine <dracine@lvlstudio.com>
2017-07-10 16:27:32 -0400

    Added more KnowMediaType to XmlConverter

--- Sources/Silphid.Loadzup/Sources/Converters/XmlConverter.cs
+++ Sources/Silphid.Loadzup/Sources/Converters/XmlConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml.Serialization;
 
@@ -6,8 +7,14 @@ namespace Silphid.Loadzup
 {
     public class XmlConverter : IConverter
     {
+        private readonly string[] _xmlMediaTypes =
+        {
+            KnownMediaType.ApplicationXml,
+            "text/xml"
+        };
+        
         public bool Supports<T>(byte[] bytes, ContentType contentType) =>
-            contentType.MediaType == KnownMediaType.ApplicationXml;
+            _xmlMediaTypes.Contains(contentType.MediaType);
 
         public T Convert<T>(byte[] bytes, ContentType contentType, Encoding encoding)
         {
