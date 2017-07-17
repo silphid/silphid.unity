5433fd152cd4f38057145eb32e9aaa276dd158e2
dracine <dracine@lvlstudio.com>
2017-07-17 14:47:06 -0400

    Added JsonConverter (Unity Built-in one)

---
+++ Sources/Silphid.Loadzup/Sources/Converters/JsonConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Silphid.Loadzup;
+using UnityEngine;
+
+public class JsonConverter : IConverter
+{
+    public bool Supports<T>(byte[] bytes, ContentType contentType) =>
+        contentType.MediaType == KnownMediaType.ApplicationJson;
+
+    public T Convert<T>(byte[] bytes, ContentType contentType, Encoding encoding)
+    {
+        return JsonUtility.FromJson<T>(encoding.GetString(bytes));
+    }
+}
