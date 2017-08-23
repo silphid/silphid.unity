4bd3982b5a9abc7b403c06b739781cfa416850be
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-08-23 12:27:56 -0400

    Better exception handling in views' image binding.

--- Sources/Silphid.Showzup/Sources/Exceptions/BindException.cs
+++ Sources/Silphid.Showzup/Sources/Exceptions/BindException.cs
@@ -2,8 +2,16 @@
 
 namespace Silphid.Showzup
 {
-    public class BindException: Exception
+    public class BindException : Exception
     {
+        public BindException()
+        {
+        }
+
+        public BindException(string message) : base(message)
+        {
+        }
+
         public BindException(string message, Exception innerException) : base(message, innerException)
         {
         }
