65543e33c0bf71c92975e2a28edf14ba30f07bf1
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-09-21 15:16:21 -0400

    Integrating Unity version of Log4net.

--- Sources/Silphid.Extensions/Sources/Unity/UnityAppender.cs
+++ Sources/Silphid.Extensions/Sources/Unity/UnityAppender.cs
@@ -1,31 +1,31 @@
-﻿using log4net.Appender;
-using log4net.Core;
-using UnityEngine;
-
-namespace Silphid.Extensions
-{
-    public class UnityAppender : AppenderSkeleton
-    {
-        /// <inheritdoc />
-        protected override void Append(LoggingEvent loggingEvent)
-        {
-            string message = RenderLoggingEvent(loggingEvent);
-
-            if (Level.Compare(loggingEvent.Level, Level.Error) >= 0)
-            {
-                // everything above or equal to error is an error
-                Debug.LogError(message);
-            }
-            else if (Level.Compare(loggingEvent.Level, Level.Warn) >= 0)
-            {
-                // everything that is a warning up to error is logged as warning
-                Debug.LogWarning(message);
-            }
-            else
-            {
-                // everything else we'll just log normally
-                Debug.Log(message);
-            }
-        }
-    }
-}
+﻿//using log4net.Appender;
+//using log4net.Core;
+//using UnityEngine;
+//
+//namespace Silphid.Extensions
+//{
+//    public class UnityAppender : AppenderSkeleton
+//    {
+//        /// <inheritdoc />
+//        protected override void Append(LoggingEvent loggingEvent)
+//        {
+//            string message = RenderLoggingEvent(loggingEvent);
+//
+//            if (Level.Compare(loggingEvent.Level, Level.Error) >= 0)
+//            {
+//                // everything above or equal to error is an error
+//                Debug.LogError(message);
+//            }
+//            else if (Level.Compare(loggingEvent.Level, Level.Warn) >= 0)
+//            {
+//                // everything that is a warning up to error is logged as warning
+//                Debug.LogWarning(message);
+//            }
+//            else
+//            {
+//                // everything else we'll just log normally
+//                Debug.Log(message);
+//            }
+//        }
+//    }
+//}
