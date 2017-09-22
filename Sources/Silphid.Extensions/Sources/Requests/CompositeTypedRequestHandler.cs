b79847b17e99c1b80257dba9f0c3d2c324b5a980
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-09-22 07:42:18 -0400

    Migrated logging in CompositeTypedRequestHandler.

--- Sources/Silphid.Extensions/Sources/Requests/CompositeTypedRequestHandler.cs
+++ Sources/Silphid.Extensions/Sources/Requests/CompositeTypedRequestHandler.cs
@@ -1,47 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using log4net;
 using Silphid.Extensions;
-using UnityEngine;
 
 namespace Silphid.Requests
 {
     public class CompositeTypedRequestHandler : IRequestHandler
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(CompositeTypedRequestHandler));
+        
         private readonly Dictionary<Type, ITypedRequestHandler> _typedRequestHandlers;
-        private readonly ILogger _logger;
 
         public CompositeTypedRequestHandler(
-            ITypedRequestHandler[] typedRequestHandlers,
-            ILogger logger = null)
+            ITypedRequestHandler[] typedRequestHandlers)
         {
             _typedRequestHandlers = typedRequestHandlers.ToDictionary(x => x.SupportedRequestType);
-            _logger = logger;
         }
 
         public IRequest Handle(IRequest request)
         {
             while (true)
             {
-                _logger?.Log($"CompositeTypedRequestHandler received {request}");
+                Log.Debug($"CompositeTypedRequestHandler received {request}");
 
                 var handler = _typedRequestHandlers.GetValueOrDefault(request.GetType());
                 if (handler != null)
                 {
-                    _logger?.Log($"Trying to handle {request} with {handler}");
+                    Log.Debug($"Trying to handle {request} with {handler}");
                     var outRequest = handler.Handle(request);
                     
                     // Request handled successfully
                     if (outRequest == null)
                     {
-                        _logger?.Log($"{request} was handled successfully by {handler}");
+                        Log.Debug($"{request} was handled successfully by {handler}");
                         return null;
                     }
 
                     // Handler returned new request to handle?
                     if (outRequest != request)
                     {
-                        _logger?.Log($"{handler} returned new {request}");
+                        Log.Debug($"{handler} returned new {request}");
                         request = outRequest;
                         continue;
                     }
