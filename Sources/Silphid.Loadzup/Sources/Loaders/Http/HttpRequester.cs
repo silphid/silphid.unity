2674ad84498781fe47dfd6e956ca4d6ff8e8a58b
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-09-28 12:33:29 -0400

    Reformatted HttpRequester.

--- Sources/Silphid.Loadzup/Sources/Loaders/Http/HttpRequester.cs
+++ Sources/Silphid.Loadzup/Sources/Loaders/Http/HttpRequester.cs
@@ -29,26 +29,31 @@ namespace Silphid.Loadzup.Http
                 .DoOnError(ex => Log.Error($"Failed GET {uri}", ex))
                 .Select(www => new Response(www.downloadHandler.data, GetMeaningfulHeaders(www.GetResponseHeaders())));
 
-        public IObservable<Response> Get(Uri uri, Options options = null) => Request(uri, options);
+        public IObservable<Response> Get(Uri uri, Options options = null) =>
+            Request(uri, options);
 
-        public IObservable<Response> Post(Uri uri, WWWForm form, Options options = null) => ObservableWebRequest
-            .Post(uri.AbsoluteUri, form, options?.RequestHeaders)
-            .DoOnSubscribe(() => Log.Info($"POST {uri}{NewLine}Form: {form}{NewLine}Headers: {options?.RequestHeaders}"))
-            .DoOnError(ex => Log.Error($"Failed POST {uri}{NewLine}Form: {form}{NewLine}Headers: {options?.RequestHeaders}", ex))
-            .Select(www => new Response(www.downloadHandler.data, GetMeaningfulHeaders(www.GetResponseHeaders())));
+        public IObservable<Response> Post(Uri uri, WWWForm form, Options options = null) =>
+            ObservableWebRequest
+                .Post(uri.AbsoluteUri, form, options?.RequestHeaders)
+                .DoOnSubscribe(() =>
+                    Log.Info($"POST {uri}{NewLine}Form: {form}{NewLine}Headers: {options?.RequestHeaders}"))
+                .DoOnError(ex =>
+                    Log.Error($"Failed POST {uri}{NewLine}Form: {form}{NewLine}Headers: {options?.RequestHeaders}", ex))
+                .Select(www => new Response(www.downloadHandler.data, GetMeaningfulHeaders(www.GetResponseHeaders())));
 
-        public IObservable<Response> Put(Uri uri, string body, Options options = null) => ObservableWebRequest
-            .Put(uri.AbsoluteUri, body, options?.RequestHeaders)
-            .DoOnSubscribe(() => Log.Info($"PUT {uri}{NewLine}Body: {body}{NewLine}Headers: {options?.RequestHeaders}"))
-            .DoOnError(ex => Log.Error($"Failed PUT {uri}{NewLine}Body: {body}{NewLine}Headers: {options?.RequestHeaders}", ex))
-            .Select(www => new Response(www.downloadHandler.data, GetMeaningfulHeaders(www.GetResponseHeaders())));
+        public IObservable<Response> Put(Uri uri, string body, Options options = null) =>
+            ObservableWebRequest
+                .Put(uri.AbsoluteUri, body, options?.RequestHeaders)
+                .DoOnSubscribe(() =>
+                    Log.Info($"PUT {uri}{NewLine}Body: {body}{NewLine}Headers: {options?.RequestHeaders}"))
+                .DoOnError(ex =>
+                    Log.Error($"Failed PUT {uri}{NewLine}Body: {body}{NewLine}Headers: {options?.RequestHeaders}", ex))
+                .Select(www => new Response(www.downloadHandler.data, GetMeaningfulHeaders(www.GetResponseHeaders())));
 
-        private Dictionary<string, string> GetMeaningfulHeaders(IDictionary<string, string> allHeaders)
-        {
-            return MeaningfulHeaders
+        private Dictionary<string, string> GetMeaningfulHeaders(IDictionary<string, string> allHeaders) =>
+            MeaningfulHeaders
                 .Select(x => new KeyValuePair<string, string>(x, allHeaders.GetValueOrDefault(x)))
                 .Where(x => x.Value != null)
                 .ToDictionary(x => x.Key, x => x.Value);
-        }
     }
 }
