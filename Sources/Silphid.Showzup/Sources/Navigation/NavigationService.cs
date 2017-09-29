5458191b06624ba8a89249e7108cf308a36f4902
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-09-29 16:45:11 -0400

    Focus now follows Selection (if Selection was set outside Focus context). Added comments and refactored for more clarity.

--- Sources/Silphid.Showzup/Sources/Navigation/NavigationService.cs
+++ Sources/Silphid.Showzup/Sources/Navigation/NavigationService.cs
@@ -101,33 +101,43 @@ namespace Silphid.Showzup.Navigation
 
         private void OnSelectionChanged(Tuple<GameObject, GameObject> change)
         {
+            // Deselect old game object
             change.Item1?
                 .GetComponents<ISelectable>()
                 .ForEach(x => x.IsSelected.Value = false);
                 
+            // Select new game object
             change.Item2?
                 .GetComponents<ISelectable>()
                 .ForEach(x => x.IsSelected.Value = true);
             
-            OnChanged(
+            // Update IsSelfOrDescendantSelected on self and ancestors
+            UpdateSelfAndAncestors(
                 SelectionAndAncestors,
                 change,
                 (obj, value) => obj?
                     .GetComponents<ISelectable>()
                     .ForEach(x => x.IsSelfOrDescendantSelected.Value = value));
+
+            // Ensure focus follows selection (should typically be the other way around,
+            // but might happen if selection is set outside of focus for some reason) 
+            Focus.Value = change.Item2;
         }
         
         private void OnFocusChanged(Tuple<GameObject, GameObject> change)
         {
+            // Defocus old game object
             change.Item1?
                 .GetComponents<IFocusable>()
                 .ForEach(x => x.IsFocused.Value = false);
                 
+            // Focus new game object
             change.Item2?
                 .GetComponents<IFocusable>()
                 .ForEach(x => x.IsFocused.Value = true);
             
-            OnChanged(
+            // Update IsSelfOrDescendantFocused on self and ancestors
+            UpdateSelfAndAncestors(
                 FocusAndAncestors,
                 change,
                 (obj, value) => obj?
@@ -135,31 +145,45 @@ namespace Silphid.Showzup.Navigation
                     .ForEach(x => x.IsSelfOrDescendantFocused.Value = value));
         }
 
-        private void OnChanged(ReactiveProperty<GameObject[]> property, Tuple<GameObject, GameObject> change, Action<GameObject, bool> update)
+        /// <summary>
+        /// Used to update either ISelectable.IsSelfOrDescendantSelected or IFocusable.IsSelfOrDescendantFocused.
+        /// </summary>
+        /// <param name="selfAndAncestorsProperty">Either SelectionAndAncestors or FocusAndAncestors reactive property</param>
+        /// <param name="change">A tuple containing old and new items</param>
+        /// <param name="update">The action to call for each old and new item to update its value.</param>
+        private void UpdateSelfAndAncestors(ReactiveProperty<GameObject[]> selfAndAncestorsProperty, Tuple<GameObject, GameObject> change, Action<GameObject, bool> update)
         {
-            var oldAncestors = property.Value;
-            var newAncestors = change.Item2?.SelfAndAncestors().Reverse().ToArray() ?? new GameObject[]{};
+            // Determine lists of old and new items
+            var oldItems = selfAndAncestorsProperty.Value;
+            var newItems = change.Item2?.SelfAndAncestors().Reverse().ToArray() ?? new GameObject[]{};
 
-            var isCommonTrunk = true; 
-            for (int i = 0; i < oldAncestors.Length || i < newAncestors.Length; i++)
+            // Goes through old and new lists in parallel, finds where old and new items
+            // diverge into two branches, and updates old items to false and new items to true
+            // (but only in their respective diverging branches, to improve performance).
+            var isDiverged = false; 
+            for (int i = 0; i < oldItems.Length || i < newItems.Length; i++)
             {
-                var oldAncestor = i < oldAncestors.Length ? oldAncestors[i] : null;
-                var newAncestor = i < newAncestors.Length ? newAncestors[i] : null;
-                
-                if (isCommonTrunk && oldAncestor == null || newAncestor == null || oldAncestor != newAncestor)
-                    isCommonTrunk = false;
+                // Get old and new items at current position 
+                var oldItem = i < oldItems.Length ? oldItems[i] : null;
+                var newItem = i < newItems.Length ? newItems[i] : null;
 
-                if (!isCommonTrunk)
+                // Detect if branches have diverged
+                if (!isDiverged && oldItem == null || newItem == null || oldItem != newItem)
+                    isDiverged = true;
+
+                // Only update items passed diverging point
+                if (isDiverged)
                 {
-                    if (oldAncestor != null)
-                        update(oldAncestor, false);
+                    if (oldItem != null)
+                        update(oldItem, false);
 
-                    if (newAncestor != null)
-                        update(newAncestor, true);
+                    if (newItem != null)
+                        update(newItem, true);
                 }
             }
             
-            property.Value = newAncestors;
+            // Update reactive property with complete list of new items (not just diverging branch)
+            selfAndAncestorsProperty.Value = newItems;
         }
 
         #endregion
