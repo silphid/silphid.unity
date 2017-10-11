e236d3f7e64056245fcc4fa1937461c5a91e767b
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-10-11 19:29:39 -0400

    Working on cache expiry.

--- Sources/Silphid.Loadzup/Sources/Loaders/Caching/CacheStorage.cs
+++ Sources/Silphid.Loadzup/Sources/Loaders/Caching/CacheStorage.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using log4net;
 using Silphid.Extensions;
+using UniRx;
 using UnityEngine;
 
 namespace Silphid.Loadzup.Caching
@@ -12,8 +14,14 @@ namespace Silphid.Loadzup.Caching
     public class CacheStorage : ICacheStorage
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(CacheStorage));
-        
-        public void Clear()
+
+        private const string HeadersExtension = ".Headers";
+        private string _cacheDir;
+
+        public IObservable<Unit> DeleteExpired(DateTime utcNow, TimeSpan defaultExpirySpan) =>
+            Observable.Start(() => DeleteExpiredInternal(utcNow, defaultExpirySpan), Scheduler.ThreadPool);
+
+        public void DeleteAll()
         {
             Directory
                 .GetFiles(GetCacheDir())
@@ -25,9 +33,35 @@ namespace Silphid.Loadzup.Caching
         public byte[] Load(Uri uri) =>
             File.ReadAllBytes(GetFilePath(uri));
 
-        public Dictionary<string, string> LoadHeaders(Uri uri)
+        public Dictionary<string, string> LoadHeaders(Uri uri) =>
+            LoadHeaders(GetHeadersFile(GetFilePath(uri)));
+
+        private void DeleteExpiredInternal(DateTime utcNow, TimeSpan defaultExpirySpan)
         {
-            var headersFile = GetHeadersFile(GetFilePath(uri));
+            int deletedCount = 0;
+            
+            Directory
+                .GetFiles(GetCacheDir())
+                .Where(file => file.EndsWith(HeadersExtension))
+                .ForEach(headersFile =>
+                {
+                    var mainFile = headersFile.RemoveSuffix(HeadersExtension);
+                    var fileDate = File.GetLastWriteTimeUtc(mainFile);
+                    var headers = LoadHeaders(headersFile);
+
+                    if (IsExpired(headers, utcNow, fileDate, defaultExpirySpan))
+                    {
+                        File.Delete(mainFile);
+                        File.Delete(headersFile);
+                        deletedCount++;
+                    }
+                });
+            
+            Log.Debug($"Deleted {deletedCount} expired files.");
+        }
+
+        private Dictionary<string, string> LoadHeaders(string headersFile)
+        {
             if (!File.Exists(headersFile))
                 return null;
 
@@ -41,19 +75,64 @@ namespace Silphid.Loadzup.Caching
                 .ToDictionary(x => x.Key, x => x.Value);
         }
 
+        private bool IsExpired(Dictionary<string, string> headers, DateTime utcNow, DateTime fileDate, TimeSpan defaultExpirySpan)
+        {
+            var cacheControl = headers.GetValueOrDefault("cache-control");
+            if (cacheControl != null)
+            {
+                var maxAge = GetMaxAge(cacheControl);
+                if (maxAge != null)
+                {
+                    var requestDate = ParseDateTime(headers.GetValueOrDefault("date")) ?? fileDate;
+                    return utcNow >= requestDate + maxAge;
+                }
+            }
+            
+            var expires = ParseDateTime(headers.GetValueOrDefault("expires")) ?? fileDate + defaultExpirySpan;
+            return utcNow >= expires;
+        }
+
+        private TimeSpan? GetMaxAge(string cacheControl)
+        {
+            const string maxAge = "max-age=";
+            int startIndex = cacheControl.IndexOf(maxAge, StringComparison.Ordinal);
+            if (startIndex == -1)
+                return null;
+
+            int endIndex = cacheControl.IndexOf(',', startIndex + maxAge.Length);
+            if (endIndex == -1)
+                endIndex = cacheControl.Length;
+
+            int length = endIndex - startIndex;
+            string secondsStr = cacheControl.Substring(startIndex, length);
+            int seconds;
+            if (!int.TryParse(secondsStr, out seconds))
+                return null;
+
+            return TimeSpan.FromSeconds(seconds);
+        }
+
+        private DateTime? ParseDateTime(string str) =>
+            str != null
+                ? DateTime.ParseExact(str,
+                    "ddd, dd MMM yyyy HH:mm:ss 'UTC'",
+                    CultureInfo.InvariantCulture.DateTimeFormat,
+                    DateTimeStyles.AssumeUniversal)
+                : (DateTime?) null;
+
         public void Save(Uri uri, byte[] bytes, IDictionary<string, string> headers)
         {
             var filePath = GetFilePath(uri);
             Log.Debug($"Save cache to: {filePath}");
             File.WriteAllBytes(filePath, bytes);
-            File.WriteAllLines(GetHeadersFile(filePath), headers.Select(x => $"{x.Key}: {x.Value}").ToArray());
+            File.WriteAllLines(GetHeadersFile(filePath), headers.Select(x => $"{x.Key.ToLower()}: {x.Value}").ToArray());
         }
 
         private string GetFilePath(Uri uri) =>
             GetCacheDir() + Path.DirectorySeparatorChar + GetEscapedFileName(uri);
 
         private string GetHeadersFile(string filePath) =>
-            filePath + ".Headers";
+            filePath + HeadersExtension;
 
         private int GetSeparatorIndex(string line)
         {
@@ -65,15 +144,18 @@ namespace Silphid.Loadzup.Caching
 
         private string GetCacheDir()
         {
-            var path = Application.temporaryCachePath +
+            if (_cacheDir != null)
+                return _cacheDir;
+            
+            _cacheDir = Application.temporaryCachePath +
                       Path.DirectorySeparatorChar +
                       Application.version +
                       Path.DirectorySeparatorChar +
                       "Silphid.Loadzup.Cache";
 
-            Log.Debug($"Cache path: {path}");
-            Directory.CreateDirectory(path);
-            return path;
+            Log.Debug($"Cache path: {_cacheDir}");
+            Directory.CreateDirectory(_cacheDir);
+            return _cacheDir;
         }
 
         private string GetEscapedFileName(Uri uri)
--- Sources/Silphid.Loadzup/Sources/Loaders/Caching/ICacheStorage.cs
+++ Sources/Silphid.Loadzup/Sources/Loaders/Caching/ICacheStorage.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using UniRx;
 
 namespace Silphid.Loadzup.Caching
 {
     public interface ICacheStorage
     {
-        void Clear();
+        IObservable<Unit> DeleteExpired(DateTime now, TimeSpan defaultExpirySpan);
+        void DeleteAll();
         bool Contains(Uri uri);
         byte[] Load(Uri uri);
         Dictionary<string, string> LoadHeaders(Uri uri);
