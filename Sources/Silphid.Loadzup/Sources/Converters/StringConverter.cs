7f200491ea0c0283d1ce6c722cbadc1ecba7e2da
fgibeau <fgibeau@lvlstudio.com>
2017-11-07 13:01:50 -0500

    Added StringConverter.

---
+++ Sources/Silphid.Loadzup/Sources/Converters/StringConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using UniRx;
+
+namespace Silphid.Loadzup
+{
+    public class StringConverter : ConverterBase<string>
+    {
+        public StringConverter() : base("text/html")
+        {
+        }
+
+        protected override IObservable<T> ConvertInternal<T>(string input, ContentType contentType, Encoding encoding) =>
+            Observable.Return((T) (object) input);
+    }
+}
