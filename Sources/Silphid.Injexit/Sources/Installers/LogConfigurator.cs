07e9771ad5514a3ddf443bc1e4858cf5d3e3754d
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-11-23 17:53:21 -0500

    Externalized log config loading into LogConfigurator. Improved log config loading to allow for easy override in DataPath.

---
+++ Sources/Silphid.Injexit/Sources/Installers/LogConfigurator.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using log4net;
+using UnityEngine;
+using log4net.Config;
+using UniRx;
+
+namespace Silphid.Injexit
+{
+    public class LogConfigurator
+    {
+        private const string FileName = "Log4net";
+        
+        private static string DataPath =>
+            #if UNITY_EDITOR
+            Path.Combine(Application.dataPath, "../AppData");
+            #else
+            Application.persistentDataPath;
+            #endif
+
+        private string ConfigDataPath =>
+            Path.Combine(DataPath, $"{FileName}.xml");
+
+        private string ConfigResourcePath =>
+            Path.Combine(Environment.CurrentDirectory, $"Assets/Resources/{FileName}.xml");
+
+        public void Configure()
+        {
+            Load();
+
+            #if UNITY_EDITOR
+            Watch();
+            #endif
+        }
+
+        #if UNITY_EDITOR
+        
+        private void Watch()
+        {
+            var path = GetPathToWatch();
+            if (path == null)
+            {
+                Debug.LogWarning($"Log config file not found for watching changes: {path}");
+                return;
+            }
+            
+            Debug.Log($"Watching log config file for changes: {path}");
+
+            Observable
+                .Interval(TimeSpan.FromSeconds(1))
+                .Select(_ => File.GetLastWriteTime(path))
+                .DistinctUntilChanged()
+                .Skip(1)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Reloading changes made to log config file");
+                    LoadFromResources();
+                });
+        }
+
+        private string GetPathToWatch()
+        {
+            if (File.Exists(ConfigDataPath))
+                return ConfigDataPath;
+
+            if (File.Exists(ConfigResourcePath))
+                return ConfigResourcePath;
+
+            return null;
+        }
+        
+        #endif
+
+        private void Load()
+        {
+            var text = LoadFromDataPath() ??
+                       LoadFromResources();
+            
+            text = text.Replace("${DataPath}", DataPath);
+            var xmldoc = new XmlDocument();
+            xmldoc.LoadXml(text);
+
+            var repository = LogManager.GetRepository(GetType().Assembly);
+            XmlConfigurator.Configure(repository, xmldoc.DocumentElement);
+        }
+
+        private string LoadFromDataPath()
+        {
+            var path = ConfigDataPath;
+            if (File.Exists(path))
+            {
+                Debug.Log($"Loading log config from DataPath: {ConfigDataPath}");
+                return File.ReadAllText(path);
+            }
+            return null;
+        }
+
+        private string LoadFromResources()
+        {
+            Debug.Log("Loading log config from resources");
+            return Resources.Load<TextAsset>(FileName).text;
+        }
+    }
+}
--- Sources/Silphid.Injexit/Sources/Installers/RootInstaller.cs
+++ Sources/Silphid.Injexit/Sources/Installers/RootInstaller.cs
@@ -1,23 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Xml;
-using log4net;
-using UnityEngine;
-using log4net.Config;
-using UniRx;
-
-namespace Silphid.Injexit
+﻿namespace Silphid.Injexit
 {
     public abstract class RootInstaller : Installer
     {
-        public string LogResourceFile = "Log4net";
-        public string LogPathToWatchForChanges = "Assets/Resources/Log4net.xml";
-
-        private static string DataPath =>
-            Application.isEditor
-                ? Path.Combine(Application.dataPath, "../AppData")
-                : Application.persistentDataPath;
-
         protected override IContainer CreateContainer() =>
             new Container(new Reflector(), ShouldInject);
 
@@ -27,52 +11,6 @@ namespace Silphid.Injexit
             return ns == null ||
                    !ns.StartsWith("Unity") &&
                    !ns.StartsWith("TMPro");
-        }
-
-        public override void Start()
-        {
-            Debug.Log("Configuring logging");
-            ConfigureLogging();
-            
-            base.Start();
-        }
-
-        protected virtual void ConfigureLogging()
-        {
-            LoadLogConfig();
-
-            if (Application.isEditor)
-            {
-                var fullPath = Path.Combine(Environment.CurrentDirectory, LogPathToWatchForChanges);
-                if (File.Exists(fullPath))
-                {
-                    Debug.Log($"Watching log config file for changes: {fullPath}");
-
-                    Observable
-                        .Interval(TimeSpan.FromSeconds(1))
-                        .Select(_ => File.GetLastWriteTime(fullPath))
-                        .DistinctUntilChanged()
-                        .Skip(1)
-                        .Subscribe(_ =>
-                        {
-                            Debug.Log("Reloading changes made to log config file");
-                            LoadLogConfig();
-                        });
-                }
-                else
-                    Debug.LogWarning($"Log config file not found for watching for changes: {fullPath}");
-            }
-        }
-
-        private void LoadLogConfig()
-        {
-            var textAsset = Resources.Load<TextAsset>(LogResourceFile);
-            var text = textAsset.text.Replace("${DataPath}", DataPath);
-            var xmldoc = new XmlDocument();
-            xmldoc.LoadXml(text);
-
-            var repository = LogManager.GetRepository(GetType().Assembly);
-            XmlConfigurator.Configure(repository, xmldoc.DocumentElement);
         }
     }
 }
