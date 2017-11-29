c185cf8099605986df91200e3862ac85e3915b7a
Mathieu Frenette <Silphid@users.noreply.github.com>
2017-11-29 11:34:32 -0500

    Fixed warnings and bug in InvalidMappingException.

--- Sources/Silphid.Showzup/Sources/Exceptions/InvalidMappingException.cs
+++ Sources/Silphid.Showzup/Sources/Exceptions/InvalidMappingException.cs
@@ -5,17 +5,11 @@ namespace Silphid.Showzup
     public class InvalidMappingException : Exception
     {
         private readonly Mapping _mapping;
-        
-        public InvalidMappingException(Mapping mapping)
-        {
-        }
 
-        public InvalidMappingException(Mapping mapping, string message) : base(message)
+        public InvalidMappingException(Mapping mapping, string message = null, Exception innerException = null)
+            : base(message, innerException)
         {
-        }
-
-        public InvalidMappingException(Mapping mapping, string message, Exception innerException) : base(message, innerException)
-        {
+            _mapping = mapping;
         }
 
         public override string Message => $"{base.Message} Mapping: {_mapping}";
