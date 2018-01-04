f2d77ede59c83b22ac5de62a9b9f12f6af4aac61
Mathieu Frenette <Silphid@users.noreply.github.com>
2018-01-04 17:48:18 -0500

    Added .At() extension method and AtSequencer decorator to support timeline-like sequencing.

--- Sources/Silphid.Sequencit/Sources/Extensions/ISequencerExtensions.cs
+++ Sources/Silphid.Sequencit/Sources/Extensions/ISequencerExtensions.cs
@@ -51,10 +51,12 @@ namespace Silphid.Sequencit
         public static object AddAction(this ISequencer This, Action action) =>
             This.Add(() => new Instant(action));
 
-        // Adds an item that pauses sequencing until a given disposable is disposed.
-        // It returns that disposable immediately, so that you store it and dispose
-        // it at any point in time. You can even dispose it before the gate is
-        // reached in the sequence.
+        /// <summary>
+        /// Adds an item that pauses sequencing until a given disposable is disposed.
+        /// It returns that disposable immediately, so that you store it and dispose
+        /// it at any point in time. You can even dispose it before the gate is
+        /// reached in the sequence.
+        /// </summary>
         public static IDisposable AddLapse(this ISequencer This)
         {
             var lapse = Lapse.Create();
@@ -62,19 +64,23 @@ namespace Silphid.Sequencit
             return lapse;
         }
 
-        // Adds a gate that pauses sequencing until a given disposable is disposed.
-        // It passes that disposable to a lambda expression that will be invoked
-        // only when the gate is reached in the sequence, so that you may then
-        // invoke some operation/animation/tween and finally dispose the disposable
-        // once completed.
+        /// <summary>
+        /// Adds a gate that pauses sequencing until a given disposable is disposed.
+        /// It passes that disposable to a lambda expression that will be invoked
+        /// only when the gate is reached in the sequence, so that you may then
+        /// invoke some operation/animation/tween and finally dispose the disposable
+        /// once completed.
+        /// </summary>
         public static object AddLapse(this ISequencer This, Action<IDisposable> action) =>
             This.Add(() => Lapse.Create(action));
 
-        // Adds a gate that pauses sequencing indefinitely when last emitted
-        // value of an observable is false and resumes sequencing immediately
-        // when it becomes true. It is recommended to use a BehaviorSubject or
-        // a ReactiveProperty, because they always emit their current value
-        // upon subscription.
+        /// <summary>
+        /// Adds a gate that pauses sequencing indefinitely when last emitted
+        /// value of an observable is false and resumes sequencing immediately
+        /// when it becomes true. It is recommended to use a BehaviorSubject or
+        /// a ReactiveProperty, because they always emit their current value
+        /// upon subscription.
+        /// </summary>
         public static object AddGate(this ISequencer This, IObservable<bool> gate) =>
             This.Add(() => gate.WhereTrue().Take(1));
 
@@ -99,5 +105,21 @@ namespace Silphid.Sequencit
         
         public static object AddDispose(this ISequencer This, IDisposable disposable) =>
             This.AddAction(disposable.Dispose);
+        
+        /// <summary>
+        /// On a Parallel sequencer, allows to schedule an event to happen at a specific
+        /// moment, specified in seconds from the start of the Parallel. Ex:
+        /// <code>
+        /// Parallel.Create(p =>
+        /// {
+        ///     p.At(0.5).Add(Completable1);
+        ///     p.At(1).Add(Completable2);
+        ///     p.At(1.5).Add(Completable3);
+        /// });
+        /// </code>
+        /// Note: Can *only* be used on a Parallel sequencer.
+        /// </summary>
+        public static ISequencer At(this ISequencer This, float timeInSeconds) =>
+            new AtSequencer(This, timeInSeconds);
     }
 }
