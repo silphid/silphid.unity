dfcb81d8653b597c61ce0d37c6e254fb3a78772d
Mathieu Frenette <Silphid@users.noreply.github.com>
2018-01-15 18:48:48 -0500

    Implemented authentication against Purple API. Now loading program (flashcards) from API instead of local file.

--- Sources/Silphid.Commons/Sources/Extensions/System/AssemblyExtensions.cs
+++ Sources/Silphid.Commons/Sources/Extensions/System/AssemblyExtensions.cs
@@ -8,6 +8,9 @@ namespace Silphid.Extensions
     public static class AssemblyExtensions
     {
         public static IEnumerable<Type> GetConcreteTypesOf<T>(this Assembly This) =>
-            This.GetTypes().Where(x => !x.IsAbstract && x.IsAssignableTo<T>());
+            This.GetTypes().Where(x =>
+                !x.IsAbstract &&
+                 !x.IsGenericTypeDefinition &&
+                 x.IsAssignableTo<T>());
     }
 }
