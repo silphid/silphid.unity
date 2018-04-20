7710c3e610cbd1cb08340ffd3c68195d918bcd71
dracine <dracine@lvlstudio.com>
2018-04-20 16:16:32 -0400

    Implemented InheritanceDepthPenalty for candidateInterface.

--- Sources/Silphid.Showzup/Sources/ScoreEvaluator.cs
+++ Sources/Silphid.Showzup/Sources/ScoreEvaluator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Silphid.Extensions;
+
 // ReSharper disable PossibleMultipleEnumeration
 
 namespace Silphid.Showzup
@@ -14,15 +15,16 @@ namespace Silphid.Showzup
         public const int IncorrectImplicitVariantPenality = 5;
         public const int TypeScore = 80;
         public const int InheritanceDepthPenality = 5;
-        public const int InterfaceDepthPenality = 2;
+        public const int InterfacePenality = 2;
         public const int FallbackVariantScore = 50;
         public const int ExcessVariantPenality = 5;
 
-        public int? GetVariantScore(VariantSet requestedVariants, VariantSet candidateVariants, VariantSet candidateImplicitVariants)
+        public int? GetVariantScore(VariantSet requestedVariants, VariantSet candidateVariants,
+            VariantSet candidateImplicitVariants)
         {
             var score = ZeroScore;
             var excessVariants = new HashSet<IVariant>(candidateVariants);
-            
+
             foreach (var requestedVariant in requestedVariants)
             {
                 // Matches exact variant?
@@ -31,7 +33,7 @@ namespace Silphid.Showzup
                     score += ExplicitVariantScore;
                     excessVariants.Remove(requestedVariant);
                 }
-                
+
                 // Matches another variant in same group? (fail!)
                 else if (candidateVariants.Any(x => x.Group == requestedVariant.Group))
                     return null;
@@ -39,11 +41,11 @@ namespace Silphid.Showzup
                 // Matches implicit variant?
                 else if (candidateImplicitVariants.Contains(requestedVariant))
                     score += ImplicitVariantScore;
-                
+
                 // Matches another implicit variant in same group?
                 else if (candidateImplicitVariants.Any(x => x.Group == requestedVariant.Group))
                     score -= IncorrectImplicitVariantPenality;
-                
+
                 // No variant specified for that group (is a fallback)
                 else
                     score += FallbackVariantScore;
@@ -79,19 +81,23 @@ namespace Silphid.Showzup
 
         private static int? GetInterfaceScore(Type candidateInterface, Type requestedType)
         {
-            var score = TypeScore - InterfaceDepthPenality;
-            IEnumerable<Type> interfaces = requestedType.GetInterfaces().ToList();
-            
-            while (interfaces.Any())
-            {
-                if (interfaces.Contains(candidateInterface))
-                    return score;
+            const int score = TypeScore - InterfacePenality;
 
-                score -= InheritanceDepthPenality;
-                interfaces = interfaces.SelectMany(x => x.GetInterfaces());
+            if (!requestedType.GetInterfaces().Contains(candidateInterface))
+                return null;
+
+            var type = requestedType.BaseType;
+            var inheritanceDepth = 0;
+
+            while (type != null)
+            {
+                if (type.GetInterfaces().Contains(candidateInterface))
+                    inheritanceDepth++;
+
+                type = type.BaseType;
             }
 
-            return null;
+            return score - InheritanceDepthPenality * inheritanceDepth;
         }
     }
 }
